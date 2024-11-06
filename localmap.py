a8e2328c91135a2960c1f07a7ffa91785c3538d7
Jessie <jessieguo.05@gmail.com>
2024-11-06 17:12:25 +0000

    Convert to round-down always. Refactored code to be clearer.

--- localmap.py
+++ localmap.py
@@ -14,8 +14,8 @@ e.g
 
 ---
 
-to find the interval of a cone, you just round towards 0 to the nearest multiple of STEP, 
-e.g -43.2 -> -40, 29.1 -> 20
+to find the interval of a cone, you round down to the nearest multiple of STEP, 
+e.g -43.2 -> -50, 29.1 -> 20
 '''
 
 ##### generate test data #####
@@ -28,17 +28,8 @@ for _ in range(100):
     cones.append((random.uniform(MIN, MAX), random.uniform(MIN, MAX)))
 
 # functions
-def round_down(x, step=1):
-    return int((x // step) * step)
-
-def round_up(x, step=1):
-    return int((((x // step) + (0 if x % step == 0 else 1))) * step)
-
-def round_towards(target, x, step=1): # round towards target, step defines what multiple to round to 
-    if x >= target:
-        return round_down(x, step=step)
-    if x < target:
-        return round_up(x, step=step)
+def round_down(x, interval=1):
+    return int((x // interval) * interval)
     
 def find_a_pythagoras(b, c): # find a in pythagoras theorem a^2 + b^2 = c^2
     '''
@@ -74,48 +65,50 @@ radius = 30 # radius we are interested in
 in_range = []
 checking = []
 
-#check for cones in range
+#check if specific cones are in range
 
-def get_distance(point1, point2):
+def get_distance_between_coords(point1, point2):
     delta_y = point1[1] - point2[1]
     delta_x = point1[0] - point2[0]
 
     return (delta_x**2 + delta_y**2)**1/2
 
-def coords_within_range(origin, test, range):
-    return range >= get_distance(origin, test)
+def coords_within_range(a, b, range):
+    return range >= get_distance_between_coords(a, b)
 
 print("Checking cones in range")
 print(f"Car is at: {car}")
 
 # main loop
 
-'''
-current optimisation issues:
-- checking snap point of car twice
-'''
+min_y_snap = round_down(car[1] - radius, STEP)
+max_y_snap = round_down(car[1] + radius, STEP)
 
-for i in range(2): # positive y and negative y
-    multiplier_y = (-1)**i
-    bound_y = round_towards(car[1], car[1] + radius * multiplier_y, STEP) 
-    min_y = round_towards(car[1], car[1], STEP)
-    max_y = bound_y + STEP * multiplier_y # ensure edge of range is checked
-    for y_snap in range(min_y, max_y, STEP * multiplier_y):
-        y_reach = abs(y_snap - car[1])
-        x_maxreach = find_a_pythagoras(y_reach, radius)
-        # search along y row for x values in range
-        for j in range(2): # positive x and negative x
-            multiplier_x = (-1)**j
-            bound_x = round_towards(car[0], car[0] + x_maxreach * multiplier_x, STEP)
-            min_x = round_towards(car[0], car[0], STEP)
-            max_x = bound_x + STEP * multiplier_x # ensure edge of range is checked
-            for x_snap in range(min_x, max_x, STEP * multiplier_x):
-                checking.append((x_snap, y_snap))
-                if (x_snap, y_snap) in map.keys():
-                    print(f"{map[(x_snap, y_snap)]} is a cone within the range")
-                    in_range = in_range + map[(x_snap,y_snap)]
-                    # for now its printing all cones that snap to the snap point
-                    # need to add checks for snap points at the edge of the circle
+for y_snap in range(min_y_snap + STEP, max_y_snap, STEP): #excluding edge snap-points
+    y_reach = abs(y_snap - car[1])
+    x_maxreach = find_a_pythagoras(y_reach, radius)
+
+    # search along y level for x values in range (excluding edge snap-points)
+    min_x_snap = round_down(car[0] - x_maxreach, STEP)
+    max_x_snap = round_down(car[0] + x_maxreach, STEP)
+    for x_snap in range(min_x_snap + STEP, max_x_snap, STEP):
+        checking.append((x_snap, y_snap))
+        if (x_snap, y_snap) in map.keys():
+            in_range = in_range + map[(x_snap,y_snap)]
+
+# edge snap-points, have to check if each cone is within range
+for y_snap in range(min_y_snap, max_y_snap + STEP, STEP):
+    y_reach = abs(y_snap - car[1])
+    x_maxreach = find_a_pythagoras(y_reach, radius)
+
+    min_x_snap = round_down(car[0] - x_maxreach, STEP)
+    max_x_snap = round_down(car[0] + x_maxreach, STEP)
+    for x_snap in [min_x_snap, max_x_snap]:
+        checking.append((x_snap, y_snap))
+        if (x_snap, y_snap) in map.keys():
+            for cone in map[(x_snap,y_snap)]:
+                if coords_within_range(car, cone, radius):
+                    in_range.append(cone)
 
 # plot points
 import matplotlib.pyplot as plt
